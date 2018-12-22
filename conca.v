cf03e7bd2c9a52a5e1c1ca6a0e6509b50dd7802c
t6s <tscompor@gmail.com>
2018-12-22 17:09:36 +0900

    prove Rnonneg_convex

--- conca.v
+++ conca.v
@@ -16,7 +16,17 @@ Section concavity_of_entropy.
 
 Lemma Rnonneg_convex : convex_interval (fun x => 0 <= x).
 Proof.
-Admitted.
+  rewrite /convex_interval.
+  move => x y t H H0 H1.
+  have H2 : 0 <= onem t by move : H1 => [] _ /onem_ge0.
+  have H3 : 0 = 0 + 0 by rewrite Rplus_0_r.
+  rewrite (_ : 0 = 0 + 0);
+    [apply Rplus_le_compat;
+     rewrite (_ : 0 = 0 * 0);
+     [|by rewrite Rmult_0_r| |by rewrite Rmult_0_r]
+    | by rewrite Rplus_0_r]; apply Rmult_le_compat => //=; try by apply Req_le.
+  by move : H1 => [].
+Qed.
 
 Definition Rnonneg_interval := mkInterval Rnonneg_convex.
 
