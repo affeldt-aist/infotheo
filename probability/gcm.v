81cea011e16fea258f79b122f019d10a65dc6170
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2019-08-14 17:48:48 +0900

    a few easy admits

--- probability/gcm.v
+++ probability/gcm.v
@@ -1787,25 +1787,38 @@ Definition mA : altMonad := MonadAlt.Pack (MonadAlt.Class P_delta_monadAltMixin)
 Lemma altxx A : idempotent (@Alt mA A).
 Proof. by move=> x; rewrite /Alt /= /alt joetxx. Qed.
 Lemma altC A : commutative (@Alt mA A).
-Admitted.
+Proof. by move=> a b; rewrite /Alt /= /alt /= joetC. Qed.
 
 Definition P_delta_monadAltCIMixin : MonadAltCI.class_of mA :=
   MonadAltCI.Class (MonadAltCI.Mixin altxx altC).
 Definition mACI : altCIMonad := MonadAltCI.Pack P_delta_monadAltCIMixin.
 
 Lemma choice0 A (x y : m A) : choice `Pr 0 x y = y.
-Admitted.
+Proof. by rewrite /choice conv0. Qed.
 Lemma choice1 A (x y : m A) : choice `Pr 1 x y = x.
-  (* NB: redundant given choice0 and choiceC, isnt' it? *)
-Admitted.
+  (* NB(sai): redundant given choice0 and choiceC, isnt' it? NB(rei): yes*)
+Proof. by rewrite /choice conv1. Qed.
 Lemma choiceC A p (x y : m A) : choice p x y = choice `Pr p.~ y x.
-Admitted.
+Proof. by rewrite /choice convC. Qed.
 Lemma choicemm A p : idempotent (@choice p A).
-Admitted.
+Proof. by move=> m; rewrite /choice convmm. Qed.
 Lemma choiceA A (p q r s : prob) (x y z : m A) :
   p = (r * s) :> R /\ s.~ = (p.~ * q.~)%R ->
   choice p x (choice q y z) = choice s (choice r x y) z.
-Admitted.
+Proof.
+case=> H1 H2.
+case/boolP : (r == `Pr 0) => r0.
+  have p0 : p = `Pr 0 by apply/prob_ext => /=; rewrite H1 (eqP r0) mul0R.
+  rewrite p0 choice0 (eqP r0) choice0 (_ : q = s) //; apply/prob_ext => /=.
+  by move: H2; rewrite p0 onem0 mul1R => /(congr1 onem); rewrite !onemK.
+case/boolP : (s == `Pr 0) => s0.
+  have p0 : p = `Pr 0 by apply/prob_ext => /=; rewrite H1 (eqP s0) mulR0.
+  rewrite p0 (eqP s0) 2!choice0 (_ : q = `Pr 0) ?choice0 //; apply/prob_ext.
+  move: H2; rewrite p0 onem0 mul1R (eqP s0) onem0 => /(congr1 onem).
+  by rewrite onemK onem1.
+rewrite /choice convA (@r_of_pq_is_r _ _ r s) //; congr ((_ <| _ |> _) <| _ |> _).
+by apply/prob_ext; rewrite s_of_pqE -H2 onemK.
+Qed.
 Lemma bindchoiceDl p : BindLaws.left_distributive (@Bind m) (@choice p).
 Admitted.
 
