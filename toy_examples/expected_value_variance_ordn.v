a5c37f29ddb2d25295cd5b4bfd0b361e272e5895
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2019-08-18 21:37:39 +0900

    variants of expected_value_variance

---
+++ toy_examples/expected_value_variance_ordn.v
@@ -0,0 +1,65 @@
+Require Import Reals Fourier.
+From mathcomp Require Import all_ssreflect.
+From infotheo Require Import Reals_ext ssrR Rbigop proba.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Import Prenex Implicits.
+
+(* Coq/SSReflect/MathComp, Morikita, Sect. 7.2, without inord *)
+
+Local Open Scope reals_ext_scope.
+Local Open Scope tuple_ext_scope.
+
+Definition ord1 {n} : 'I_n.+2 := lift ord0 ord0.
+Definition ord2 {n} : 'I_n.+3 := lift ord0 ord1.
+
+Lemma ord0E n : 0%nat = @ord0 n. Proof. done. Qed.
+Lemma ord1E n : 1%nat = @ord1 n. Proof. done. Qed.
+Lemma ord2E n : 2%nat = @ord2 n. Proof. done. Qed.
+
+Definition p : {ffun 'I_3 -> R} :=
+  finfun [fun x => 0 with ord0 |-> 1/2, ord1 |-> 1/3, ord2 |-> 1/6].
+
+Lemma p_nonneg : [forall a : 'I_3, 0 <b= p a].
+Proof.
+apply/forallP => a.
+rewrite /p ffunE /=.
+apply/leRP.
+do! case: ifP => _; fourier.
+Qed.
+
+Definition p' : [finType of 'I_3] ->R+ := mkPosFfun p_nonneg.
+
+Lemma p_sum1 : \sum_(i in 'I_3) p' i == 1.
+Proof.
+apply/eqP.
+rewrite 3!big_ord_recl big_ord0 addR0 /=.
+rewrite /p !ffunE /=.
+by field.
+Qed.
+
+Local Open Scope proba_scope.
+
+Definition P : {dist 'I_3} := mkDist p_sum1.
+
+Definition X : {RV P -> R} := (fun i => INR i.+1).
+
+Lemma expected : `E X = 5/3.
+Proof.
+rewrite /Ex.
+rewrite 3!big_ord_recl big_ord0 addR0 /=.
+rewrite /p /X !ffunE /= /bump /= !addn0 addn1.
+rewrite !S_INR (_ : 0%:R = 0) //.
+by field.
+Qed.
+
+Lemma variance : Var P X = 5/9.
+Proof.
+rewrite VarE expected /Ex /X /sq_RV /comp_RV /=.
+rewrite 3!big_ord_recl big_ord0 addR0 /=.
+rewrite !mul1R !mulR1.
+rewrite !ffunE /bump !addn0 /=.
+rewrite !S_INR (_ : 0%:R = 0) //.
+by field.
+Qed.
---
+++ toy_examples/expected_value_variance_tuple.v
@@ -0,0 +1,58 @@
+Require Import Reals Fourier.
+From mathcomp Require Import all_ssreflect.
+From infotheo Require Import Reals_ext ssrR Rbigop proba.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Import Prenex Implicits.
+
+(* Coq/SSReflect/MathComp, Morikita, Sect. 7.2, using tuple *)
+
+Local Open Scope reals_ext_scope.
+Local Open Scope tuple_ext_scope.
+
+Definition ps := [tuple 1/2; 1/3; 1/6].
+Definition p : {ffun 'I_3 -> R} := [ffun i => tnth ps i].
+
+Lemma p_nonneg : [forall a : 'I_3, 0 <b= p a].
+Proof.
+apply/forallP => a.
+rewrite /p ffunE /=.
+move: a; apply/all_tnthP => /=.
+do! (apply/andP; split) => //; apply/leRP; fourier.
+Qed.
+
+Definition p' : [finType of 'I_3] ->R+ := mkPosFfun p_nonneg.
+
+Lemma p_sum1 : \sum_(i in 'I_3) p' i == 1.
+Proof.
+apply/eqP.
+rewrite 3!big_ord_recl big_ord0 addR0 /=.
+rewrite /p !ffunE !(tnth_nth 0) /=.
+by field.
+Qed.
+
+Local Open Scope proba_scope.
+
+Definition P : {dist 'I_3} := mkDist p_sum1.
+
+Definition X : {RV P -> R} := (fun i => INR i.+1).
+
+Lemma expected : `E X = 5/3.
+Proof.
+rewrite /Ex.
+rewrite 3!big_ord_recl big_ord0 addR0 /=.
+rewrite /X !ffunE !(tnth_nth 0) /= /bump /= !addn0 addn1.
+rewrite !S_INR (_ : 0%:R = 0) //.
+by field.
+Qed.
+
+Lemma variance : Var P X = 5/9.
+Proof.
+rewrite VarE expected /Ex /X /sq_RV /comp_RV /=.
+rewrite 3!big_ord_recl big_ord0 addR0 /=.
+rewrite !mul1R !mulR1.
+rewrite !ffunE !(tnth_nth 0) /bump !addn0 /=.
+rewrite !S_INR (_ : 0%:R = 0) //.
+by field.
+Qed.
