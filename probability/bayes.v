7ed7b1f2c53d4c5a9785b4e17845285fac584b7a
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2020-01-27 17:18:45 +0100

    add factorization theorem

--- probability/bayes.v
+++ probability/bayes.v
@@ -36,7 +36,9 @@ Variable parent : rel 'I_n.
 Definition topological := forall i j : 'I_n, parent i j -> i < j.
 End topological.
 
-Record t :=
+(* Koller and Friedman, Definition 3.1, page 57 *)
+
+Record t := mkBN
   { vars: 'I_n -> aRV;
     parent: rel 'I_n;
     topo: topological parent;
@@ -49,3 +51,36 @@ Record t :=
         X _|_ Y | Z }.
 End bn.
 End BN.
+
+Section Factorization.
+Import BN.
+Variable U : finType.
+Variable P : fdist U.
+Variable n : nat.
+Variable bn : t P n.
+
+Definition RV_domains :=
+  [seq aRV_type (vars bn i) | i <- [tuple i | i < n]].
+
+Definition RV_domain := foldr prod_finType unit_finType RV_domains.
+
+Variant aRVV := mkRVV : forall V : aRV P, aRV_type V -> aRVV.
+
+Variables vs : 'I_n -> aRVV.
+Hypothesis vs_ok : forall i, let: mkRVV AV _ := vs i in vars bn i = AV.
+
+Definition preim_vars (I : {set 'I_n}) :=
+  \bigcap_(i < n)
+   let: mkRVV (mkRV A X) a := vs i in
+   if i \in I then X @^-1 a else setT.
+
+(* Theorem 3.1, page 62 *)
+
+Theorem BN_factorization :
+  Pr P (preim_vars setT) =
+  \big[Rmult/R1]_(i < n)
+   let parents := [set k | closure (parent bn) [set k] i] in
+   `Pr_ P [ preim_vars [set i] | preim_vars parents ].
+Abort.
+
+End Factorization.
