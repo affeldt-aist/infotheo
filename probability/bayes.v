b0632328e6f7a0cc7a4ee2384f1854af3dfd8312
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2020-01-28 22:38:40 +0100

    finish proof of cinde_preim_ok (in distinct case)

--- probability/bayes.v
+++ probability/bayes.v
@@ -35,7 +35,19 @@ Definition cinde_preim (e f g : {set 'I_n}) :=
     let G := preim_vars g vals in
     `Pr_ P [ E :&: F | G ] = `Pr_ P [ E | G ] * `Pr_ P [ F | G ].
 
-Parameter rvar_choice : forall A : finType, {RV P -> A} -> A.
+Definition rvar_choice : forall A : finType, {RV P -> A} -> A.
+move=> A X.
+move: (fdist_card_neq0 (RVar.d X)).
+move He: (enum A) => [|a l] //.
+move/(f_equal size): He.
+by rewrite -cardE => ->.
+Defined.
+
+Definition ord_eq_dec (i j : 'I_n) : {i = j}+{i <> j}.
+case (Nat.eq_dec i j); intro ij.
+- left; now apply ord_inj.
+- right; intro ij'; apply ij; now f_equal.
+Defined.
 
 Definition set_val (i : 'I_n) (v : types i) (vals : forall j, types j) :=
   fun j : 'I_n =>
@@ -44,40 +56,53 @@ Definition set_val (i : 'I_n) (v : types i) (vals : forall j, types j) :=
     | right _ => vals j
     end.
 
+Lemma eq_dec_refl i : Nat.eq_dec i i = left (erefl i).
+Proof.
+case: Nat.eq_dec => Hi; last by elim Hi.
+congr left; by rewrite (Eqdep_dec.UIP_refl_nat _ Hi).
+Qed.
+
 Lemma cinde_preim_ok (i j k : 'I_n) :
+  i <> j -> i <> k -> j <> k ->
   cinde_preim [set i] [set j] [set k] <-> vars i _|_ vars j | (vars k).
 Proof.
+move=> nij nik njk.
 rewrite /cinde_drv /cinde_preim /preim_vars.
 split.
 - move=> Hpreim a b c.
   set vals := set_val a (set_val b (set_val c (fun i => rvar_choice (vars i)))).
   move/(_ vals): Hpreim.
-  rewrite !big_set1.
-  rewrite /cPr /cPr0 !setX1.
-  rewrite !snd_RV3 !snd_RV2.
-  rewrite ![Pr _ [set _]]/Pr !big_set1.
-  rewrite /RVar.d !FDistMap.dE.
-  rewrite /Pr.
-  set lhs1 := _ / _.
-  move => Hpreim.
+  rewrite !big_set1 /cPr /cPr0 !setX1 !snd_RV3 !snd_RV2.
+  rewrite ![Pr _ [set _]]/Pr !big_set1 /RVar.d !FDistMap.dE /Pr.
+  have -> : vals i = a.
+    rewrite /vals /set_val eq_dec_refl.
+    rewrite -Eqdep_dec.eq_rect_eq_dec //; exact: ord_eq_dec.
+  have -> : vals j = b.
+    rewrite /vals /set_val eq_dec_refl.
+    case: Nat.eq_dec => ij; first by elim nij; apply ord_inj.
+    rewrite -Eqdep_dec.eq_rect_eq_dec //; exact: ord_eq_dec.
+  have -> : vals k = c.
+    rewrite /vals /set_val eq_dec_refl.
+    case: Nat.eq_dec => ik; first by elim nik; apply ord_inj.
+    case: Nat.eq_dec => jk; first by elim njk; apply ord_inj.
+    rewrite -Eqdep_dec.eq_rect_eq_dec //; exact: ord_eq_dec.
+  set lhs1 := _ / _ => Hpreim.
   set lhs2 := _ / _.
-  admit.
+  have <- : lhs1 = lhs2.
+    by congr (_ / _); apply eq_bigl => u; rewrite !inE.
+  rewrite Hpreim.
+  by congr ((_/_) * (_/_)); apply eq_bigl => u; rewrite !inE.
 - move=> Hdrv vals.
   move/(_ (vals i) (vals j) (vals k)): Hdrv.
-  rewrite !big_set1.
-  rewrite /cPr /cPr0 !setX1.
-  rewrite !snd_RV3 !snd_RV2.
-  rewrite ![Pr _ [set _]]/Pr !big_set1.
-  rewrite /RVar.d !FDistMap.dE.
-  rewrite /Pr.
-  set lhs1 := _ / _.
-  move => Hdrv.
+  rewrite !big_set1 /cPr /cPr0 !setX1 !snd_RV3 !snd_RV2.
+  rewrite ![Pr _ [set _]]/Pr !big_set1 /RVar.d !FDistMap.dE /Pr.
+  set lhs1 := _ / _ => Hdrv.
   set lhs2 := _ / _.
   have -> : lhs2 = lhs1.
     by congr (_ / _); apply eq_bigl => u; rewrite !inE.
-  rewrite {}Hdrv.
-  congr ((_/_) * (_/_)); apply eq_bigl => u; rewrite !inE //.
-Admitted.
+  rewrite Hdrv.
+  by congr ((_/_) * (_/_)); apply eq_bigl => u; rewrite !inE.
+Qed.
 End preim.
 
 Section Imap.
