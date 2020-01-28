aa8b599446c2b86de378675a5bf67e65ecaa3a9c
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2020-01-28 17:33:28 +0100

    prove half of cinde_preim_ok

--- probability/bayes.v
+++ probability/bayes.v
@@ -3,7 +3,7 @@ From mathcomp Require Import all_ssreflect ssralg fingroup perm finalg matrix.
 From mathcomp Require boolp.
 Require Import Reals. (* Lra Nsatz. *)
 Require Import ssrR Reals_ext logb ssr_ext ssralg_ext bigop_ext Rbigop.
-Require Import fdist cinde.
+Require Import fdist jfdist cinde.
 
 Local Open Scope tuple_ext_scope.
 Local Open Scope proba_scope.
@@ -22,10 +22,8 @@ Variable n : nat.
 Variable types : 'I_n -> finType.
 Variable vars : forall i, {RV P -> types i}.
 
-Section Imap.
-Variable parent : rel 'I_n.
-
-Definition topological := forall i j : 'I_n, parent i j -> i < j.
+Section preim.
+Local Open Scope R_scope.
 
 Definition preim_vars (I : {set 'I_n}) (vals : forall i, types i) :=
   \bigcap_(i in I) vars i @^-1 (vals i).
@@ -35,7 +33,36 @@ Definition cinde_preim (e f g : {set 'I_n}) :=
     let E := preim_vars e vals in
     let F := preim_vars f vals in
     let G := preim_vars g vals in
-    (`Pr_ P [ E :&: F | G ] = `Pr_ P [ E | G ] * `Pr_ P [ F | G ])%R.
+    `Pr_ P [ E :&: F | G ] = `Pr_ P [ E | G ] * `Pr_ P [ F | G ].
+
+Lemma cinde_preim_ok (i j k : 'I_n) :
+  cinde_preim [set i] [set j] [set k] <-> vars i _|_ vars j | (vars k).
+Proof.
+rewrite /cinde_drv /cinde_preim /preim_vars.
+split.
+- move=> Hpreim a b c. admit.
+- move=> Hdrv vals.
+  move/(_ (vals i) (vals j) (vals k)): Hdrv.
+  rewrite !big_set1.
+  rewrite /cPr /cPr0 !setX1.
+  rewrite !snd_RV3 !snd_RV2.
+  rewrite ![Pr _ [set _]]/Pr !big_set1.
+  rewrite /RVar.d !FDistMap.dE.
+  rewrite /Pr.
+  set lhs1 := _ / _.
+  move => Hdrv.
+  set lhs2 := _ / _.
+  have -> : lhs2 = lhs1.
+    by congr (_ / _); apply eq_bigl => u; rewrite !inE.
+  rewrite {}Hdrv.
+  congr ((_/_) * (_/_)); apply eq_bigl => u; rewrite !inE //.
+Admitted.
+End preim.
+
+Section Imap.
+Variable parent : rel 'I_n.
+
+Definition topological := forall i j : 'I_n, parent i j -> i < j.
 
 Definition independence (i j : 'I_n) :=
   ~~ closure parent [set i] j ->
@@ -62,9 +89,9 @@ Variable types : 'I_n -> finType.
 Variable vars : forall i, {RV P -> types i}.
 Variable bn : t vars.
 
-(* Theorem 3.1, page 62 *)
 Local Open Scope R_scope.
 
+(* Theorem 3.1, page 62 *)
 Theorem BN_factorization vals :
   Pr P (preim_vars vars setT vals) =
   \prod_(i < n)
