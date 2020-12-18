78b2dc5f62c559f30d3877ee23757ca5324865f1
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2020-12-18 11:02:39 +0900

    line disappeared?

--- probability/bayes.v
+++ probability/bayes.v
@@ -1,5 +1,6 @@
 (* infotheo: information theory and error-correcting codes in Coq               *)
 (* Copyright (C) 2020 infotheo authors, license: LGPL-2.1-or-later              *)
+From mathcomp Require Import all_ssreflect ssralg fingroup perm finalg matrix.
 From mathcomp Require boolp.
 Require Import Reals. (* Lra Nsatz. *)
 Require Import ssrR Reals_ext logb ssr_ext ssralg_ext bigop_ext Rbigop.
