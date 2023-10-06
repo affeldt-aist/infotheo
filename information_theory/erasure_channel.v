77b54e885d8f22cd64c0c0c8eb6e3fb53f2812af
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2023-10-06 17:32:55 +0900

    easure_channel.v

--- information_theory/erasure_channel.v
+++ information_theory/erasure_channel.v
@@ -1,10 +1,10 @@
 (* infotheo: information theory and error-correcting codes in Coq              *)
 (* Copyright (C) 2020 infotheo authors, license: LGPL-2.1-or-later             *)
-From mathcomp Require Import all_ssreflect ssralg fingroup finalg perm zmodp.
+From mathcomp Require Import all_ssreflect all_algebra fingroup finalg perm zmodp.
 From mathcomp Require Import matrix.
 Require Import Reals.
 From mathcomp Require Import Rstruct.
-Require Import ssrR Reals_ext ssr_ext ssralg_ext logb Rbigop fdist entropy.
+Require Import ssrR Reals_ext realType_ext ssr_ext ssralg_ext logb Rbigop fdist entropy.
 Require Import binary_entropy_function channel hamming channel_code.
 
 (******************************************************************************)
@@ -27,6 +27,7 @@ Module EC.
 Section EC_sect.
 Variables (A : finType) (p : R).
 Hypothesis p_01 : 0 <= p <= 1.
+Local Open Scope ring_scope.
 
 Definition f (a : A) := [ffun b =>
   if b is Some a' then
@@ -36,20 +37,22 @@ Definition f (a : A) := [ffun b =>
 Lemma f0 a b : 0 <= f a b.
 Proof.
 rewrite /f ffunE.
-case: b => [a'|]; last by case: p_01.
+case: b => [a'|]; last first.
+  by case: p_01 => /RleP.
 case: ifP => _.
-case: p_01 => ? ?; exact/onem_ge0.
-exact/leRR.
+case: p_01 => ? ?.
+  by apply/onem_ge0/RleP.
+done.
 Qed.
 
 Lemma f1 (a : A) : \sum_(a' : {:option A}) f a a' = 1.
 Proof.
 rewrite (bigD1 None) //= (bigD1 (Some a)) //= !ffunE eqxx /= (proj2 (psumR_eq0P _)).
-- by rewrite addR0 onemKC.
+- by rewrite -RplusE addR0 onemKC.
 - rewrite /f; case => [a'|]; last by case: p_01.
   rewrite ffunE.
   case: ifPn => [_ |*]; last exact/leRR.
-  case: p_01 => ? ? _; exact/onem_ge0.
+  case: p_01 => ? ? _; exact/RleP/onem_ge0/RleP.
 - case => //= a' aa'; rewrite ffunE; case: ifPn => // /eqP ?; subst a'.
   move: aa'; by rewrite eqxx.
 Qed.
@@ -60,10 +63,10 @@ Definition c : `Ch(A, [finType of option A]) :=
 End EC_sect.
 
 Section EC_prob.
-
+Local Open Scope fdist_scope.
 Variable X : finType.
 Hypothesis card_X : #|X| = 2%nat.
-Variables (P : fdist X) (p : R) (* erasure probability *).
+Variables (P : {fdist X}) (p : R) (* erasure probability *).
 Hypothesis p_01 : 0 <= p <= 1.
 
 Let BEC := @EC.c X p p_01.
