ae6146a1f41cee5702bc3844af0d9cd16099d79c
Greg Weng <greg.weng@mercari.com>
2024-05-13 18:11:56 +0900

    WIP: lemma 3.4

--- probability/smc.v
+++ probability/smc.v
@@ -5,6 +5,7 @@ Require Import Reals.
 From mathcomp Require Import Rstruct zmodp.
 Require Import ssrR Reals_ext logb ssr_ext ssralg_ext bigop_ext fdist.
 Require Import proba jfdist_cond graphoid.
+Import GRing.Theory.
 
 (******************************************************************************)
 (*                              SMC Useful Tools                              *)
@@ -86,7 +87,14 @@ End more_independent_rv_lemmas.
 
 Section lemma_3_4.
 
+
 Notation R := real_realType.
+Lemma cpr_eqE_mul (U : finType) (P : {fdist U}) (TA TB : eqType)
+  (X : {RV P -> TA}) (Y : {RV P -> TB}) a b :
+  `Pr[ X = a | Y = b ] * `Pr[Y = b] = `Pr[ [% X, Y] = (a, b) ].
+Proof.
+rewrite cpr_eqE.
+Admitted.
 
 Variable T : finType.
 Variable P : R.-fdist T.
@@ -96,24 +104,41 @@ Variables (X Y : {RV P -> 'I_p}).
 
 (* How to express "the distribution of random variable X is uniform distribution" as a prop. *)
 Variable pX_unif : `p_ X = fdist_uniform (card_ord p).
-Variable pY_unif : `p_ X = fdist_uniform (card_ord p).
+Variable pY_unif : `p_ Y = fdist_uniform (card_ord p).
 Variable XY_indep : inde_rv X Y.
 
 (* Add two random variables = add results from two functions. *)
-Definition XY : {RV P -> 'I_p} := fun x => Zp_add (X x) (Y x).
+Definition XY : {RV P -> 'I_p} := fun x => (X x + Y x)%mcR.
+
+(* Map between random variables *)
+
+(* Goal: a similar lemma for `Pr [X+Y in E] = \sum ... [X \in K `* Y \in I - K]*)
+(* Need: get `i` from I so we can have i - k??*)
+Fail Lemma reasoning_by_cases_XY:
+  `Pr[ XY \in I ] = \sum_(k <- fin_img X) `Pr[ [% X, Y] \in ([set k] `* [set ik])].
+
 
 Lemma pXY_unif : `p_ XY = fdist_uniform (card_ord p).
 Proof.
-apply: fdist_ext=> /= x.
+apply: fdist_ext=> /= i.
 rewrite fdist_uniformE /=.
-rewrite fdistmapE /=.
-under eq_bigr=> i /=.
-  rewrite !inE /XY /=.
-(* need a lemma to transit from Line 1 to Line 2 of the chained equations *)
-  (* if we follow the paper lines, we need to have a k, and i - k in goal 2 ,
-     and split XY by marginal? But this is different from [% X, Y] in lemma_3_2 *)
+transitivity (`Pr[XY \in [set i]]).
+  rewrite pr_inE'.
+  admit.
+rewrite (reasoning_by_cases _ X).
+transitivity (\sum_(k <- fin_img X) `Pr[ [% X, Y] \in ([set k] `* [set i-k]%mcR) ]).
+  apply eq_bigr=> k _.
+  rewrite !pr_eq_setE.
+  rewrite /Pr.
+  apply: eq_bigl.
+  move=>r /=.
+  rewrite !inE /=.
+  rewrite /XY.
+  rewrite andbC; apply: andb_id2l.
+  move /eqP ->.
+  rewrite [RHS]eq_sym.
+  by rewrite subr_eq addrC eq_sym.
+under eq_bigr do rewrite setX1 pr_eq_set1 -cpr_eqE_mul.
 Abort.
-
-Lemma 
 
 End lemma_3_4.
