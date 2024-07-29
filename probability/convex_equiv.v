f99f249fee456f7f3cb8c211b0a2c45c23fb1bfc
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2024-07-29 19:39:31 +0900

    convex_equiv.v

--- probability/convex_equiv.v
+++ probability/convex_equiv.v
@@ -292,10 +292,10 @@ Module B := NaryToBin(A).
 Module EA := Equiv2(A).
 Import A B.
 
-Let equiv_convn n (d : {fdist 'I_n}) (g : 'I_n -> A.T) : <&>_d g = <|>_d g.
+Definition equiv_convn n (d : {fdist 'I_n}) (g : 'I_n -> A.T) : <&>_d g = <|>_d g.
 Proof. by []. Qed.
 
-Let T' := NaryConv_sort__canonical__convex_ConvexSpace.
+Definition T' := NaryConv_sort__canonical__convex_ConvexSpace.
 
 Lemma equiv_conv p (a b : C.T) : a <| p |> b = a <& p &> b.
 Proof.
