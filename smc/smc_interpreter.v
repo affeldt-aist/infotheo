4b40d3bb8cae3aef79be73931f0a1fbb7509c331
Greg Weng <greg.weng@mercari.com>
2024-12-07 19:23:40 +0900

    Debug logs

--- smc/smc_interpreter.v
+++ smc/smc_interpreter.v
@@ -36,31 +36,33 @@ Inductive proc : Type :=
   | Fail : proc.
 
 Inductive log : Type :=
-  | Log : nat -> data -> log.
+  | Log : nat -> nat -> data -> log.
 
-Definition step (A : Type) (ps : seq proc) (logs : seq log) (yes no : proc -> A) (logger : proc -> seq log -> seq log) (i : nat) : A * seq log:=
+Definition step (A : Type) (ps : seq proc) (logs : seq log) (yes no : proc -> A)
+  (logger : nat -> data -> seq log -> seq log)
+  (i : nat) : A * seq log:=
   let p := nth Fail ps i in
     if p is Recv frm f then
         if nth Fail ps frm is Send dst v _ then
-          if dst == i then (no (f v), logger p logs) else (no p, logger p logs)
-        else (no p, logger p logs)
+          if dst == i then (no (f v), logger i v logs) else (no p, logs)
+        else (no p, logs)
     else if p is Send dst w next then
       if nth Fail ps dst is Recv frm _ then
-        if frm == i then (yes next, logger p logs) else (no p, logger p logs)
-      else (no p, logger p logs)
+        if frm == i then (yes next, logs) else (no p, logs)
+      else (no p, logs)
     else
-      (no p, logger p logs).
+      (no p, logs).
 About step.
 
 (* Extract log in Send because only in send we have both dst and v: data;
    in Recv we only have f, no data.
 *)
 Fixpoint interp h (ps : seq proc) (logs : seq log) :=
-  let logger :=  (fun p logs => if p is Send dst v _ then Log dst v :: logs else logs) in
+  let logger := (fun round => (fun pid d logs => Log round pid d :: logs)) in
   if h is h.+1 then
-    if has (fun i => fst (@step bool ps [::] (fun=>true) (fun=>false) logger i))
+    if has (fun i => fst (@step bool ps [::] (fun=>true) (fun=>false) (logger h) i))
         (iota 0 (size ps)) then
-      let pslogs' := map (@step proc ps [::] idfun idfun logger) (iota 0 (size ps)) in
+      let pslogs' := map (@step proc ps [::] idfun idfun (logger h)) (iota 0 (size ps)) in
       let ps' := unzip1 pslogs' in
       let logs' := unzip2 pslogs' in
         interp h ps' ((foldr (fun pclogs acc => pclogs ++ acc) logs) logs')
@@ -76,14 +78,12 @@ Variable P : R.-fdist T.
 Variable dotproduct : VX -> VX -> TX.
 Notation "u *d w" := (dotproduct u w).
 
-Print add_RV.
-
 Definition alice : nat := 0.
 Definition bob : nat := 1.
 Definition coserv : nat := 2.
 (*Note: notation will make cbv fail.*)
 
-Definition data := option ({RV P -> TX} + {RV P -> VX}).
+Definition data := option (TX + VX).
 Definition one x : data := Some (inl x).
 Definition vec x : data := Some (inr x).
 Definition Recv_one frm f : proc data :=
@@ -91,64 +91,60 @@ Definition Recv_one frm f : proc data :=
 Definition Recv_vec frm f : proc data :=
   Recv frm (fun x => if x is Some (inr v) then f v else Ret None).
 
-Definition palice (xa : {RV P -> VX}) : proc data :=
+Definition palice (xa : VX) : proc data :=
   Recv_vec coserv (fun sa =>
   Recv_one coserv (fun ra =>
-  Send bob (vec (xa \+ sa)) (
+  Send bob (vec (xa + sa)) (
   Recv_vec bob (fun xb' =>
   Recv_one bob (fun t =>
-  Ret (one (t \- (xb' \*d sa) \+ ra))))))).
-Definition pbob (xb : {RV P -> VX}) : proc data :=
+  Ret (one (t - (xb' *d sa) + ra))))))).
+Definition pbob (xb : VX) (yb : TX) : proc data :=
   Recv_vec coserv (fun sb =>
-  Recv_one coserv (fun yb =>
   Recv_one coserv (fun rb =>
   Recv_vec alice (fun xa' =>
-  let t := xa' \*d xb \+ rb \- yb in
-    Send alice (vec (xb \+ sb))
-    (Send alice (one t) (Ret (one yb))))))).
-Definition pcoserv (sa sb: {RV P -> VX}) (ra yb: {RV P -> TX}) : proc data :=
+  let t := xa' *d xb + rb - yb in
+    Send alice (vec (xb + sb))
+    (Send alice (one t) (Ret (one yb)))))).
+Definition pcoserv (sa sb: VX) (ra : TX) : proc data :=
   Send alice (vec sa) (
   Send alice (one ra) (
   Send bob (vec sb) (
-  Send bob (one yb) (
-  Send bob (one (sa \*d sb \- ra)) (Ret None))))).
+  Send bob (one (sa *d sb - ra)) (Ret None)))).
 
-Variables (sa sb: {RV P -> VX}) (ra yb: {RV P -> TX}) (xa xb: {RV P -> VX}).
+Variables (sa sb: VX) (ra yb: TX) (xa xb: VX).
 Definition scalar_product h :=
-  interp h [:: palice xa; pbob xb; pcoserv sa sb ra yb] [::].
+  interp h [:: palice xa; pbob xb yb; pcoserv sa sb ra] [::].
 
 About scalar_product.
 
 Goal scalar_product 8 = ([:: (Fail _); (Fail _); (Fail _)], [::]).
 cbv -[GRing.add GRing.opp GRing.Ring.sort].
-fold bob alice.
+Fail rewrite [X in Log _ X _]/alice.
+(*fold coserv bob alice.*)
 Undo 2.
 rewrite /scalar_product.
 rewrite (lock (8 : nat)) /=.
 rewrite -lock (lock (7 : nat)) /=.
 rewrite -lock (lock (6 : nat)) /=.
 rewrite -lock (lock (5 : nat)) /=.
+rewrite -lock (lock (4 : nat)) /=.
+rewrite -lock (lock (3 : nat)) /=.
 Abort.
 
 (* Bug: the first Send is not in the logs *)
 Lemma scalar_product_ok :
   scalar_product 8 =
-  ([:: Ret (one ((xa \+ sa) \*d xb \+ (sa \*d sb \- ra) \- yb \- (xb \+ sb) \*d sa \+ ra));
+  ([:: Ret (one ((xa + sa) *d xb + (sa *d sb - ra) - yb - (xb + sb) *d sa + ra));
        Ret (one yb);
        Ret None]
-  , [::
-       Log alice (Some (inl ((xa \+ sa) \*d xb \+ (sa \*d sb \- ra) \- yb)));
-       Log alice (Some (inr (xb \+ sb)));
-       Log bob (Some (inr (xa \+ sa)));
-       Log bob (Some (inr (xa \+ sa)));
-       Log bob (Some (inl (sa \*d sb \- ra)));
-       Log bob (Some (inr (xa \+ sa)));
-       Log bob (Some (inl yb));
-       Log bob (Some (inr (xa \+ sa)));
-       Log bob (Some (inr sb));
-       Log alice (Some (inl ra));
-       Log alice (Some (inr sa))
-    ]).
+  ,[:: Log 1 0 (Some (inl ((xa + sa) *d xb + (sa *d sb - ra) - yb)));
+       Log 2 0 (Some (inr (xb + sb)));
+       Log 3 1 (Some (inr (xa + sa)));
+       Log 4 1 (Some (inl (sa *d sb - ra)));
+       Log 5 1 (Some (inr sb));
+       Log 6 0 (Some (inl ra));
+       Log 7 0 (Some (inr sa))
+  ]).
 Proof. reflexivity. Qed.
 
 End scalar_product.
@@ -156,39 +152,41 @@ End scalar_product.
 Section information_leakage_proof.
 
 Variable TX VX : ringType.
-Variable T : finType.
-Variable P : R.-fdist T.
 
 Record alice_view :=
   AliceView {
-    x1  : option ({RV P -> VX} + unit);
-    s1  : option ({RV P -> VX} + unit);
-    r1  : option ({RV P -> TX} + unit);
-    x2' : option ({RV P -> VX} + unit);
-    t   : option ({RV P -> TX} + unit);
-    y1  : option ({RV P -> TX} + unit);
+    x1  : option (VX + unit);
+    s1  : option (VX + unit);
+    r1  : option (TX + unit);
+    x2' : option (VX + unit);
+    t   : option (TX + unit);
+    y1  : option (TX + unit);
   }.
 
-Definition set_x1 (view :alice_view) (v : {RV P -> VX}) : alice_view :=
+Definition set_x1 (view :alice_view) (v : VX) : alice_view :=
   AliceView (Some (inl v)) (s1 view) (r1 view) (x2' view) (t view) (y1 view).
 
-Definition set_s1 (view :alice_view) (v : {RV P -> VX}) : alice_view :=
+Definition set_s1 (view :alice_view) (v : VX) : alice_view :=
   AliceView (x1 view) (Some (inl v)) (r1 view) (x2' view) (t view) (y1 view).
 
-Definition set_r1 (view :alice_view) (v : {RV P -> TX}) : alice_view :=
+Definition set_r1 (view :alice_view) (v : TX) : alice_view :=
   AliceView (x1 view) (s1 view) (Some (inl v)) (x2' view) (t view) (y1 view).
 
-Definition set_x2' (view :alice_view) (v : {RV P -> VX}) : alice_view :=
+Definition set_x2' (view :alice_view) (v : VX) : alice_view :=
   AliceView (x1 view) (s1 view) (r1 view) (Some (inl v)) (t view) (y1 view).
 
-Definition set_t (view :alice_view) (v : {RV P -> TX}) : alice_view :=
+Definition set_t (view :alice_view) (v : TX) : alice_view :=
   AliceView (x1 view) (s1 view) (r1 view) (x2' view) (Some (inl v)) (y1 view).
 
-Definition set_y1 (view :alice_view) (v : {RV P -> TX}) : alice_view :=
+Definition set_y1 (view :alice_view) (v : TX) : alice_view :=
   AliceView (x1 view) (s1 view) (r1 view) (x2' view) (y1 view) (Some (inl v)).
 
-Fixpoint build_alice_view (logs : seq (log (data TX VX P))) (result : option (alice_view + unit)) : option (alice_view + unit) :=
-  if result is Some (inl view) then
+Definition build_alice_view (logs : seq (log (data TX VX))) (acc : option (alice_view + unit)) : option (alice_view + unit) :=
+  let s1 := nth (inr tt) logs 0 in
+  let r1 := nth (inr tt) logs 1 in
+  foldr 
+
+  if acc is Some (inl view) then
     (* Each log line can only provide one field -- we need a temporary building view here.
     *)
     Some (inl view)
