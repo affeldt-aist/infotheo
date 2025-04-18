b32f02ba14d89274fc954d0f8138d958b763183d
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2025-04-18 17:39:24 +0900

    inde_rv -> inde_RV

--- smc/smc_proba.v
+++ smc/smc_proba.v
@@ -54,16 +54,16 @@ Proof. by rewrite fdistmapE; apply: eq_bigl=> ?; rewrite !inE. Qed.
 
 End more_fdist.
 
-Section more_inde_rv.
+Section more_inde_RV.
 Context {R : realType}.
 Variables (A : finType) (P : R.-fdist A) (TA TB : finType).
 Variables (X : {RV P -> TA}) (Y : {RV P -> TB}).
 
-Definition inde_rv_ev :=
+Definition inde_RV_ev :=
   forall E F,
     `Pr[ [% X, Y] \in E `* F] = `Pr[ X \in E ] * `Pr[ Y \in F ].
 
-Lemma inde_rv_events' : inde_rv X Y <-> inde_rv_ev.
+Lemma inde_RV_events' : P |= X _|_ Y <-> inde_RV_ev.
 Proof.
 split=> H; last by move=> *; rewrite -!pr_in1 -H setX1.
 move=> E F; rewrite !pr_inE'.
@@ -80,14 +80,14 @@ rewrite -big_setX; apply: eq_bigr=> *.
 by rewrite fdistmapE.
 Qed.
 
-End more_inde_rv.
+End more_inde_RV.
 
 Lemma preimg_tt {R : realType} {T TY : finType} (P : R.-fdist T)
     (Y : {RV P -> TY}) (y : TY) :
   [% unit_RV P, Y] @^-1: [set (tt, y)] = Y @^-1: [set y].
 Proof. by apply/setP => ?; rewrite !inE. Qed.
 
-Section more_independent_rv_lemmas.
+Section more_inde_RV_lemmas.
 Context {R : realType}.
 Variables (A : finType) (P : R.-fdist A) (TA TB TC TD : finType).
 Variables (X : {RV P -> TA}) (Y : {RV P -> TB}) (Z : {RV P -> TC}).
@@ -98,10 +98,11 @@ Local Notation "f × g" :=
 
 (* Information-Theoretically Secure Number Protocol*)
 (* Lemma 3.1 *)
-Lemma inde_rv_comp (UB' TB' : finType) (g' : TB' -> UB')(Y' : {RV P -> TB'}): P|= X _|_ Y' -> P|= (f `o X) _|_ (g' `o Y').
+Lemma inde_RV_comp (UB' TB' : finType) (g' : TB' -> UB') (Y' : {RV P -> TB'}) :
+  P |= X _|_ Y' -> P|= (f `o X) _|_ (g' `o Y').
 Proof.
-move/inde_rv_events'.
-rewrite /inde_rv_ev.
+move/inde_RV_events'.
+rewrite /inde_RV_ev.
 move=> H i j.
 rewrite -[LHS]pr_in1.
 rewrite comp_RV2_ACA /=.
@@ -117,13 +118,13 @@ Lemma inde_RV2_comp : P|= X _|_ [% Y, Z] -> P|= (f `o X) _|_ [% (g `o Y), (h `o 
 Proof.
 pose gh := (fun '(y, z) => (g y, h z)).
 have ->: [% g `o Y, h `o Z] = gh `o [%Y, Z] by [].
-apply inde_rv_comp.
+exact: inde_RV_comp.
 Qed.
 
 (* Lemma 3.2 *)
-Lemma RV2_inde_rv_snd : P |= [% X, Y] _|_ Z -> P |= Y _|_ Z.
+Lemma RV2_inde_RV_snd : P |= [% X, Y] _|_ Z -> P |= Y _|_ Z.
 Proof.
-move/inde_rv_events'.
+move/inde_RV_events'.
 move=> H y z.
 rewrite -[LHS]pr_in1 pr_inE'.
 rewrite -(snd_RV2 X [% Y, Z]) Pr_fdist_snd.
@@ -150,8 +151,8 @@ Qed.
 (* Lemma 3.3 *)
 Lemma inde_RV2_cinde : P |= [% X, Z] _|_ Y -> X _|_ Y | Z.
 Proof.
-move/cinde_rv_unit /cinde_rv_sym.
-move/weak_union /cinde_rv_sym.
+move=> /cinde_RV_unit/cinde_RV_sym.
+move=> /weak_union /cinde_RV_sym.
 by apply cpr_prd_unit_RV.
 Qed.
 
@@ -165,9 +166,10 @@ have [Y0|?] := eqVneq `Pr[ Y = b ] 0.
 by rewrite mulVf// mulr1.
 Qed.
 
-Lemma inde_rv_cprP : P |= X _|_ Y <-> forall x y, `Pr[ Y = y ] != 0 -> `Pr[ X = x | Y = y] = `Pr[ X = x].
+Lemma inde_rv_cprP : P |= X _|_ Y <->
+  forall x y, `Pr[ Y = y ] != 0 -> `Pr[ X = x | Y = y] = `Pr[ X = x].
 Proof.
-rewrite /inde_rv.
+rewrite /inde_RV.
 split.
   move => H x y Hy.
   move/(_ x y):H. (* bring back H and apply it with x and y*)
@@ -181,7 +183,9 @@ case /boolP: (`Pr[ Y = y0 ] == 0) => Hy.
 by rewrite H.
 Qed.
 
-Lemma cinde_rv_cprP : P |= X _|_ Y | Z <-> forall x y z, `Pr[[%Y, Z] = (y, z) ] != 0 -> `Pr[ X = x | [%Y, Z] = (y, z)] = `Pr[ X = x | Z = z].
+Lemma cinde_rv_cprP : P |= X _|_ Y | Z <->
+  forall x y z, `Pr[[%Y, Z] = (y, z) ] != 0 ->
+    `Pr[ X = x | [%Y, Z] = (y, z)] = `Pr[ X = x | Z = z].
 Proof.
 split.
   move => H x y z YZne0.
@@ -191,18 +195,19 @@ split.
 move => H x y z.
 move: (H x y z) => H2.
 apply: inde_RV2_cinde.
-rewrite /inde_rv => [[_a _b] _c].
+rewrite /inde_RV => [[_a _b] _c].
 Abort.
 
-End more_independent_rv_lemmas.
+End more_inde_RV_lemmas.
 
 Section XY.
 Context {R : realType}.
 Variables (A : finType) (P : R.-fdist A) (TA TB: finType).
 Variables (X : {RV P -> TA}) (Y : {RV P -> TB}).
 
-Lemma inde_rv_sym:  P|= X _|_ Y <-> P|= Y _|_ X.
-Proof. by split => /cinde_rv_unit/cinde_rv_sym/cinde_rv_unit. Qed.
+(* TODO: move to proba.v *)
+Lemma inde_RV_sym : P |= X _|_ Y <-> P |= Y _|_ X.
+Proof. by split => /cinde_RV_unit/cinde_RV_sym/cinde_RV_unit. Qed.
 
 End XY.
 
@@ -211,8 +216,10 @@ Context {R : realType}.
 Variables (A : finType) (P : R.-fdist A) (TA TB TC: finType).
 Variables (X : {RV P -> TA}) (Y : {RV P -> TB}) (Z : {RV P -> TC}).
 
-Lemma inde_RV2_sym : P|=[%X, Y] _|_ Z <-> P|=[%Y, X] _|_ Z.
-Proof. by split => /cinde_rv_unit/cinde_rv_sym/cinde_drv_2C/cinde_rv_sym/cinde_rv_unit. Qed.
+Lemma inde_RV2_sym : P |= [%X, Y] _|_ Z <-> P |= [%Y, X] _|_ Z.
+Proof.
+by split => /cinde_RV_unit/cinde_RV_sym/cinde_drv_2C/cinde_RV_sym/cinde_RV_unit.
+Qed.
 
 End XYZ.
 
@@ -304,12 +311,12 @@ Hypothesis EX' : X' = X :> (T -> TX).
 Lemma Pr_fdist_cond_RV x : `Pr[ X' = x ] = `Pr[ X = x | Y = y ].
 Proof. by rewrite pr_eqE_finType Pr_fdist_cond cPr_eq_finType EX'. Qed.
 
-Hypothesis Z_XY_indep : inde_rv Z [%X, Y].
+Hypothesis Z_XY_indep : P |= Z _|_ [%X, Y].
 
 Lemma fdist_cond_indep : fdist_cond E0 |= X _|_ Z.
 Proof.
-move: Z_XY_indep => /cinde_rv_unit /weak_union.
-rewrite /cinde_rv /= => H.
+move: Z_XY_indep => /cinde_RV_unit /weak_union.
+rewrite /cinde_RV /= => H.
 move => /= x z.
 rewrite mulrC pr_eq_pairC.
 have := H z x (tt,y).
@@ -326,12 +333,12 @@ Let XZ : {RV P -> TZ} := X `+ Z.
 
 Variable Z_XY_indep : P |= Z _|_ [%X, Y].
 
-Let Z_X_indep : inde_rv Z X.
-Proof. exact/cinde_rv_unit/decomposition/cinde_rv_unit/Z_XY_indep. Qed.
+Let Z_X_indep : P |= Z _|_ X.
+Proof. exact/cinde_RV_unit/decomposition/cinde_RV_unit/Z_XY_indep. Qed.
 
-Let Z_Y_indep : inde_rv Z Y.
+Let Z_Y_indep : P |= Z _|_ Y.
 Proof.
-exact/cinde_rv_unit/decomposition/cinde_drv_2C/cinde_rv_unit/Z_XY_indep.
+exact/cinde_RV_unit/decomposition/cinde_drv_2C/cinde_RV_unit/Z_XY_indep.
 Qed.
 
 Variable n : nat.
@@ -364,7 +371,7 @@ rewrite -big_distrl /=.
 rewrite /X' cPr_1 ?mul1r//; last by rewrite pr_eqE_finType.
 rewrite pr_eqE' (add_RV_unif X Z (card_TZ)) //.
 - by rewrite fdist_uniformE.
-- rewrite /inde_rv /= => /= z0 z1.
+- rewrite /inde_RV /= => /= z0 z1.
   by rewrite pr_eq_pairC/= Z_X_indep/= mulrC.
 Qed.
 
@@ -390,7 +397,7 @@ Variables (X2 : {RV P -> TY}) (X1 : {RV P -> TX}) (Xn Z : {RV P -> TZ}).
 
 Hypothesis card_TZ : #|TZ| = n.+1.
 Variable pZ_unif : `p_ Z = fdist_uniform card_TZ.
-Variable Z_Xs_indep : inde_rv Z [%X1, X2, Xn].
+Variable Z_Xs_indep : P |= Z _|_ [%X1, X2, Xn].
 
 Let XnZ := Xn `+ Z.
 
@@ -404,11 +411,11 @@ apply: cinde_alt; last exact: X0.
 rewrite (inde_RV2_sym X1 X2 XnZ) in H.
 apply: H.
 rewrite inde_RV2_sym.
-rewrite inde_rv_sym.
+rewrite inde_RV_sym.
 apply: (@lemma_3_5' _ _ _ _ P Xn Z [% X1, X2] _ _ _ pZ_unif).
-apply/cinde_rv_unit.
+apply/cinde_RV_unit.
 apply: cinde_drv_2C.
-exact/cinde_rv_unit.
+exact/cinde_RV_unit.
 Qed.
 
 End lemma_3_6.
@@ -417,10 +424,10 @@ Section theorem_3_7.
 Context {R : realType}.
 Variables (T TX TY1 TY2 : finType)(TZ : finZmodType).
 Variable P : R.-fdist T.
-Variables (X: {RV P -> TX}) (Z : {RV P -> TZ}).
+Variables (X : {RV P -> TX}) (Z : {RV P -> TZ}).
 Variables (f1 : TX -> TY1) (f2 : TX -> TY2) (fm : TX -> TZ).
 
-Variable Z_X_indep : inde_rv Z X.
+Variable Z_X_indep : P |= Z _|_ X.
 
 Let Y1 := f1 `o X.
 Let Y2 := f2 `o X.  (* y2...ym-1*)
@@ -444,7 +451,7 @@ apply.
   exact: pZ_unif.
 rewrite (_ : [%_ , _] = Y) //.
 rewrite (_ : Z = idfun `o Z) //. (* id vs. idfun*)
-exact: inde_rv_comp.
+exact: inde_RV_comp.
 Qed.
 
 (*TODO: the Entropy part needs to be done in another file, not inside the probability directory. *)
