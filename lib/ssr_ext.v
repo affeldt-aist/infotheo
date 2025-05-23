e13c629782020a37361bf6f3b1a77b0a75f90792
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2025-05-23 23:58:42 +0900

    rm order prefix

--- lib/ssr_ext.v
+++ lib/ssr_ext.v
@@ -1217,7 +1217,9 @@ End fintype_extra.
 Section order_extra.
 (* eq_le would be a better name but it is already occupied:
    eq_le : (x == y) = (x <= y <= x)%O *)
-Lemma eqW {disp : order.Order.disp_t} {T : porderType disp} (x y : T) :
+
+Lemma eqW {disp : Order.disp_t} {T : porderType disp} (x y : T) :
   x = y -> (x <= y)%O.
 Proof. by move->; exact: Order.POrderTheory.lexx. Qed.
+
 End order_extra.
